5fe4e4899a788c0a06942857521e80738a1a5596
vlad <vlad.devlikamov@gmail.com>
2021-03-09 11:41:32 +0300

    add task_1

---
+++ modules/task_1/devlikamov_v_integrate_rec/integrate_rec.h
@@ -0,0 +1,7 @@
+// Copyright 2021 Devlikamov Vladislav
+#ifndef MODULES_TASK_1_DEVLIKAMOV_V_INTEGRATE_REC_INTEGRATE_REC_H_
+#define MODULES_TASK_1_DEVLIKAMOV_V_INTEGRATE_REC_INTEGRATE_REC_H_
+
+double integral(double a1, double b1, double a2, double b2, double h, double (*f)(double, double));
+
+#endif  // MODULES_TASK_1_DEVLIKAMOV_V_INTEGRATE_REC_INTEGRATE_REC_H_
