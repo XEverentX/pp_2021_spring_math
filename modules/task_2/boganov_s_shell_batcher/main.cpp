d44beed46b6604480c37e95dbe84377bd0ac610e
Frixinglife <frixinglife@gmail.com>
2021-03-29 21:25:47 +0300

    fix tests

--- modules/task_2/boganov_s_shell_batcher/main.cpp
+++ modules/task_2/boganov_s_shell_batcher/main.cpp
@@ -8,7 +8,7 @@
 TEST(SortShellSeq, sequential_shell_sorting_test_for_10_thousands) {
   int number_of_parts = 4;
   std::vector<int> vec = getRandomVector(10000);
-  std::vector<int> res = SortShellOMP(vec, number_of_parts);
+  std::vector<int> res = SortShellSeq(vec, number_of_parts);
 
   std::sort(vec.begin(), vec.end());
   ASSERT_EQ(vec, res);
@@ -23,6 +23,15 @@ TEST(SortShellOMP, OMP_shell_sorting_test_for_10_thousands) {
   ASSERT_EQ(vec, res);
 }
 
+TEST(SortShellSeq, sequential_shell_sorting_test_for_100_thousands) {
+  int number_of_parts = 4;
+  std::vector<int> vec = getRandomVector(100000);
+  std::vector<int> res = SortShellSeq(vec, number_of_parts);
+
+  std::sort(vec.begin(), vec.end());
+  ASSERT_EQ(vec, res);
+}
+
 TEST(SortShellOMP, OMP_shell_sorting_test_for_100_thousands) {
   int number_of_parts = 4;
   std::vector<int> vec = getRandomVector(100000);
@@ -32,25 +41,16 @@ TEST(SortShellOMP, OMP_shell_sorting_test_for_100_thousands) {
   ASSERT_EQ(vec, res);
 }
 
-TEST(SortShellSeq, sequential_shell_sorting_test_for_100_thousands) {
+TEST(SortShellSeq, sequential_shell_sorting_test_for_1_million) {
   int number_of_parts = 4;
-  std::vector<int> vec = getRandomVector(100000);
-  std::vector<int> res = SortShellOMP(vec, number_of_parts);
+  std::vector<int> vec = getRandomVector(1000000);
+  std::vector<int> res = SortShellSeq(vec, number_of_parts);
 
   std::sort(vec.begin(), vec.end());
   ASSERT_EQ(vec, res);
 }
 
 TEST(SortShellOMP, OMP_shell_sorting_test_for_1_million) {
-  int number_of_parts = 4;
-  std::vector<int> vec = getRandomVector(1000000);
-  std::vector<int> res = SortShellOMP(vec, number_of_parts);
-
-  std::sort(vec.begin(), vec.end());
-  ASSERT_EQ(vec, res);
-}
-
-TEST(SortShellSeq, sequential_shell_sorting_test_for_1_million) {
   int number_of_parts = 4;
   std::vector<int> vec = getRandomVector(1000000);
   std::vector<int> res = SortShellOMP(vec, number_of_parts);
