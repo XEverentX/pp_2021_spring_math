d1350b771b18ac8b0ab287f9e361c70e41d30105
Alexander Lebedev <Inok.Alex@gmail.com>
2021-05-14 06:53:28 +0300

    Лебедев Александр. Задача 1. Метод Симпсона.

---
+++ modules/task_1/lebedev_a_simpson/simpson.h
@@ -0,0 +1,16 @@
+// Copyright 2021 Lebedev Alexander
+#ifndef MODULES_TASK_1_LEBEDEV_A_SIMPSON_SIMPSON_H_
+#define MODULES_TASK_1_LEBEDEV_A_SIMPSON_SIMPSON_H_
+
+#include <functional>
+#include <cstdint>
+
+double integrate(std::function<double(double, double)> f,
+                 double a,
+                 double b,
+                 double c,
+                 double d,
+                 uint32_t nx,
+                 uint32_t ny);
+
+#endif  // MODULES_TASK_1_LEBEDEV_A_SIMPSON_SIMPSON_H_
